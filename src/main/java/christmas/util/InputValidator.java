d74345c0e1215690de61cfa780a9951fae774cc5
whoamixzerone <whoamixzerone@gmail.com>
2023-11-12 23:58:28 +0900

    feat(promotion): 최대 주문 20개를 초과하는 경우 예외 처리 구현

--- src/main/java/christmas/util/InputValidator.java
+++ src/main/java/christmas/util/InputValidator.java
@@ -12,10 +12,12 @@ public class InputValidator {
 
     private static final String INVALID_DAY = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     private static final String INVALID_ORDER = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private static final String INVALID_TOTAL_MENU_COUNT = "[ERROR] 최대 주문 개수는 20개입니다. 다시 입력해 주세요.";
 
     private static final int FROM_DAY = 1;
     private static final int TO_DAY = 31;
-    private static final int MENU_COUNT = 1;
+    private static final int DEFAULT_MENU_COUNT = 1;
+    private static final int TOTAL_MENU_COUNT = 20;
 
     public static void validNumber(String input) {
         validBlank(input);
@@ -65,6 +67,7 @@ public class InputValidator {
     public static void validOrder(Map<String, Integer> menus) {
         validMenu(menus);
         validMenuCount(menus);
+        validMenuTotalCount(menus);
     }
 
     private static void validMenu(Map<String, Integer> menus) {
@@ -86,12 +89,24 @@ public class InputValidator {
     }
 
     private static boolean isNotOneMoreThan(int menuCount) {
-        return menuCount < MENU_COUNT;
+        return menuCount < DEFAULT_MENU_COUNT;
     }
 
     public static void validOrderDuplicate(Map<String, Integer> menus, String[] menuGroup) {
         if (menus.size() != menuGroup.length) {
             throw new IllegalArgumentException(INVALID_ORDER);
         }
+    }
+
+    private static void validMenuTotalCount(Map<String, Integer> menus) {
+        if (TOTAL_MENU_COUNT < sumMenuCount(menus)) {
+            throw new IllegalArgumentException(INVALID_TOTAL_MENU_COUNT);
+        }
+    }
+
+    private static int sumMenuCount(Map<String, Integer> menus) {
+        return menus.entrySet().stream()
+                .mapToInt(menu -> menu.getValue())
+                .sum();
     }
 }
