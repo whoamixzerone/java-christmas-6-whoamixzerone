7243d8d0153cdbfdd016942331dac7f9ca3de46d
whoamixzerone <whoamixzerone@gmail.com>
2023-11-13 19:01:15 +0900

    feat(promotion): 할인 전 총주문 금액을 출력하는 기능 구현

--- src/main/java/christmas/domain/ChristmasEvent.java
+++ src/main/java/christmas/domain/ChristmasEvent.java
@@ -2,18 +2,27 @@ package christmas.domain;
 
 import christmas.constants.Menu;
 import christmas.view.InputView;
+import christmas.view.OutputView;
 
 import java.util.Map;
 
 public class ChristmasEvent {
     private final InputView inputView;
+    private final OutputView outputView;
+
 
     public ChristmasEvent() {
         inputView = new InputView();
+        outputView = new OutputView();
     }
 
     public void execute() {
         int reservationDay = inputView.readDate();
-        Map<Menu, Integer> menus = inputView.readOrder();
+        Map<Menu, Integer> order = inputView.readOrder();
+
+        Restaurant restaurant = new Restaurant(order);
+
+        outputView.showOrderMenu(restaurant, reservationDay);
+        outputView.showTotalAmountBeforeDiscount(restaurant);
     }
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -10,4 +10,13 @@ public class OutputView {
         System.out.println("<주문 메뉴>");
         System.out.println(restaurant);
     }
+
+    public void showTotalAmountBeforeDiscount(Restaurant restaurant) {
+        System.out.println("<할인 전 총주문 금액>");
+
+        String formatter = "%,d원";
+        System.out.println(String.format(formatter, restaurant.calculateTotalAmountBeforeDiscount()));
+
+        System.out.printf("총주문 금액 %s 이상부터 이벤트가 적용됩니다%n", String.format(formatter, DEFAULT_EVENT_AMOUNT));
+    }
 }
