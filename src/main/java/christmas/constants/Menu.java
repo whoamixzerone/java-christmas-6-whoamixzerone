37d94b96acfb112342b0dc9d49d8ab4fc5ec75df
whoamixzerone <whoamixzerone@gmail.com>
2023-11-15 12:11:30 +0900

    style(promotion): long 타입 "L" 표시

--- src/main/java/christmas/constants/Menu.java
+++ src/main/java/christmas/constants/Menu.java
@@ -3,19 +3,19 @@ package christmas.constants;
 import java.util.*;
 
 public enum Menu {
-    MUSHROOM_CREATE_SOUP("양송이수프", 6_000),
-    TAPAS("타파스", 5_500),
-    CAESAR_SALAD("시저샐러드", 8_000),
-    T_BONE_STEAK("티본스테이크", 55_000),
-    BBQ_RIBS("바비큐립", 54_000),
-    SEA_FOOD_PASTA("해산물파스타", 35_000),
-    CHRISTMAS_PASTA("크리스마스파스타", 25_000),
-    CHOCOLATE_CAKE("초코케이크", 15_000),
-    ICE_CREAM("아이스크림", 5_000),
-    COKE_ZERO("제로콜라", 3_000),
-    RED_WINE("레드와인", 60_000),
-    CHAMPAGNE("샴페인", 25_000),
-    EMPTY("없음", 0);
+    MUSHROOM_CREATE_SOUP("양송이수프", 6_000L),
+    TAPAS("타파스", 5_500L),
+    CAESAR_SALAD("시저샐러드", 8_000L),
+    T_BONE_STEAK("티본스테이크", 55_000L),
+    BBQ_RIBS("바비큐립", 54_000L),
+    SEA_FOOD_PASTA("해산물파스타", 35_000L),
+    CHRISTMAS_PASTA("크리스마스파스타", 25_000L),
+    CHOCOLATE_CAKE("초코케이크", 15_000L),
+    ICE_CREAM("아이스크림", 5_000L),
+    COKE_ZERO("제로콜라", 3_000L),
+    RED_WINE("레드와인", 60_000L),
+    CHAMPAGNE("샴페인", 25_000L),
+    EMPTY("없음", 0L);
 
     private String food;
     private long amount;
