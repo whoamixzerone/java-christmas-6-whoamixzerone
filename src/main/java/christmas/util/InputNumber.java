f2863d3577950075a6cf279ab77ace97be9daf38
whoamixzerone <whoamixzerone@gmail.com>
2023-11-15 12:46:04 +0900

    style(promotion): import 제거

--- src/main/java/christmas/util/InputNumber.java
+++ src/main/java/christmas/util/InputNumber.java
@@ -1,9 +1,5 @@
 package christmas.util;
 
-import christmas.constants.Menu;
-
-import java.util.Map;
-
 public class InputNumber {
     public static int toInt(String input) {
         return Integer.parseInt(input);
